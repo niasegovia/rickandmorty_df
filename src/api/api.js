4253dacea02924be35c79e12f0cfc53863639c87
Núbia Segóvia <rsegovia.nubia@gmail.com>
2021-10-30 08:50:00 -0300

    [ADD] api

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+const Api = {
+    apiUrl: "https://rickandmortyapi.com/api/character",
+
+    fetchGetAll: () => fetch(Api.apiUrl),
+    fetchGetById: (id) => fetch(`${Api.apiUrl}/${id}`)
+}
+
+export default Api;
